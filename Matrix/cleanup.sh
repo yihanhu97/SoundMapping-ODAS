678fb9d78bb064ddc8799ac00342107dbf037764
yihanhu97 <35671831+yihanhu97@users.noreply.github.com>
2020-06-25 00:18:25 -0700

    added cleanup.sh

---
+++ Matrix/cleanup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+rm -rf /home/pi/odas/recordings/SST/*
+rm -rf /home/pi/odas/recordings/SSL/*
+rm -rf /home/pi/odas/recordings/separated/*
+rm -rf /home/pi/odas/recordings/postfiltered/*
