738daaf946857f2de81c919da9fee5ae07b1b9c2
yihanhu97 <35671831+yihanhu97@users.noreply.github.com>
2020-06-24 23:52:28 -0700

    Update backup.sh

--- Matrix/backup.sh
+++ Matrix/backup.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-rclone copy /home/pi/odas/recordings RaspberryPi:/ODAS/recordings
+#! /bin/bash
+
+rclone copy /home/pi/odas/recordings/SST RaspberryPi:/ODAS/logs0/SST
+rclone copy /home/pi/odas/recordings/SSL RaspberryPi:/ODAS/logs0/SSL
+rclone copy /home/pi/odas/recordings/separated RaspberryPi:/ODAS/recordings0/separated
+rclone copy /home/pi/odas/recordings/postfiltered RaspberryPi:/ODAS/recordings0/postfiltered
