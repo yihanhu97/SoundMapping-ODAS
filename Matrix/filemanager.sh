99182c6671355d339a97ef57795bd655d3e5170e
yihanhu97 <35671831+yihanhu97@users.noreply.github.com>
2020-06-24 23:55:51 -0700

    update filemanager.sh

--- Matrix/filemanager.sh
+++ Matrix/filemanager.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-find /home/pi/odas/recordings -mmin +15 -exec rm {} \;
+find /home/pi/odas/recordings/SSL -mmin +15 -exec rm {} \;
+find /home/pi/odas/recordings/SST -mmin +15 -exec rm {} \;
+find /home/pi/odas/recordings/separated -mmin +15 -exec rm {} \;
+find /home/pi/odas/recordings/postfiltered -mmin +15 -exec rm {} \;
